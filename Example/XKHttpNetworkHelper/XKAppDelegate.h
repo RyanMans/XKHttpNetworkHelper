4dda04e14373876d0cf903e1f433b1e11cfba58f
chonglou <chonglou@sinoxk.com>
2019-02-14 16:35:57 +0800

    update
    update

---
+++ Example/XKHttpNetworkHelper/XKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XKAppDelegate.h
+//  XKHttpNetworkHelper
+//
+//  Created by RyanMans on 02/14/2019.
+//  Copyright (c) 2019 RyanMans. All rights reserved.
+//
+
+@import UIKit;
+
+@interface XKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/XKHttpNetworkHelper/XKViewController.h
@@ -0,0 +1,13 @@
+//
+//  XKViewController.h
+//  XKHttpNetworkHelper
+//
+//  Created by RyanMans on 02/14/2019.
+//  Copyright (c) 2019 RyanMans. All rights reserved.
+//
+
+@import UIKit;
+
+@interface XKViewController : UIViewController
+
+@end
